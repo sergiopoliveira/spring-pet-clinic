b9a533cd7ac0c2d0c69d225a55ed8b601e5ba7be
sergiopoliveira <sergiopoliveira@outlook.com>
2019-03-24 14:38:37 +0000

    Impl Map based Services, closes #6

---
+++ pet-clinic-data/src/main/java/oliveira/sergio/petclinic/service/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package oliveira.sergio.petclinic.service.map;
+
+import oliveira.sergio.petclinic.model.Pet;
+import oliveira.sergio.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
