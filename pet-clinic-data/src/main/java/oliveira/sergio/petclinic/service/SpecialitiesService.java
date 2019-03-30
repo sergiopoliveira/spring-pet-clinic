728116c1b11da2af0ad0c55fd565e79059464c33
sergiopoliveira <sergiopoliveira@outlook.com>
2019-03-30 18:19:48 +0000

    added Specialties Map Impl.closes #31

---
+++ pet-clinic-data/src/main/java/oliveira/sergio/petclinic/service/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package oliveira.sergio.petclinic.service;
+
+import oliveira.sergio.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/oliveira/sergio/petclinic/service/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package oliveira.sergio.petclinic.service.map;
+
+import oliveira.sergio.petclinic.model.Speciality;
+import oliveira.sergio.petclinic.service.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
