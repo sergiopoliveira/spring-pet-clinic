367e003a47930411d2d5752e34b35893130d6663
sergiopoliveira <sergiopoliveira@outlook.com>
2019-03-31 14:34:29 +0100

    convert owners to jpa entities. closes #36

--- pet-clinic-data/src/main/java/oliveira/sergio/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/oliveira/sergio/petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package oliveira.sergio.petclinic.model;
 
-public class PetType extends BaseEntity{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     String name;
 
     public String getName() {
